c0410ae42129aadb327181c7a63876ac863dc7ac
Bob <bob@mymusclechef.com>
2022-09-12 10:36:38 +1000

    upgrade ios sdk version

--- ios/RNBraintreeDropIn.m
+++ ios/RNBraintreeDropIn.m
@@ -12,33 +12,34 @@ RCT_EXPORT_MODULE(RNBraintreeDropIn)
 
 RCT_EXPORT_METHOD(show:(NSDictionary*)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-
-    if([options[@"darkTheme"] boolValue]){
-        BTUIKAppearance.sharedInstance.colorScheme = BTUIKColorSchemeDark;
+    BTDropInUICustomization *uiCustomization;
+    if ([options[@"darkTheme"] boolValue]) {
+        uiCustomization = [[BTDropInUICustomization alloc] initWithColorScheme:BTDropInColorSchemeDark];
     } else {
-        BTUIKAppearance.sharedInstance.colorScheme = BTUIKColorSchemeLight;
+        uiCustomization = [[BTDropInUICustomization alloc] initWithColorScheme:BTDropInColorSchemeLight];
     }
-
-    if(options[@"fontFamily"]){
-        [BTUIKAppearance sharedInstance].fontFamily = options[@"fontFamily"];
+    
+    if (options[@"fontFamily"]) {
+        uiCustomization.fontFamily = options[@"fontFamily"];
     }
-    if(options[@"boldFontFamily"]){
-        [BTUIKAppearance sharedInstance].boldFontFamily = options[@"boldFontFamily"];
+    if (options[@"boldFontFamily"]) {
+        uiCustomization.boldFontFamily = options[@"boldFontFamily"];
     }
-
+    
     self.resolve = resolve;
     self.reject = reject;
     self.applePayAuthorized = NO;
-
+    
     NSString* clientToken = options[@"clientToken"];
     if (!clientToken) {
         reject(@"NO_CLIENT_TOKEN", @"You must provide a client token", nil);
         return;
     }
-
+    
     BTDropInRequest *request = [[BTDropInRequest alloc] init];
     request.cardholderNameSetting = BTFormFieldRequired;
-
+    request.uiCustomization = uiCustomization;
+    
     NSDictionary* threeDSecureOptions = options[@"threeDSecure"];
     if (threeDSecureOptions) {
         NSNumber* threeDSecureAmount = threeDSecureOptions[@"amount"];
@@ -46,30 +47,29 @@ RCT_EXPORT_METHOD(show:(NSDictionary*)options resolver:(RCTPromiseResolveBlock)r
             reject(@"NO_3DS_AMOUNT", @"You must provide an amount for 3D Secure", nil);
             return;
         }
-
-        request.threeDSecureVerification = YES;
+        
         BTThreeDSecureRequest *threeDSecureRequest = [[BTThreeDSecureRequest alloc] init];
         threeDSecureRequest.amount = [NSDecimalNumber decimalNumberWithString:threeDSecureAmount.stringValue];
         request.threeDSecureRequest = threeDSecureRequest;
-
+        
     }
-
+    
     BTAPIClient *apiClient = [[BTAPIClient alloc] initWithAuthorization:clientToken];
     self.dataCollector = [[BTDataCollector alloc] initWithAPIClient:apiClient];
-    [self.dataCollector collectCardFraudData:^(NSString * _Nonnull deviceDataCollector) {
+    [self.dataCollector collectDeviceData:^(NSString * _Nonnull deviceDataCollector) {
         // Save deviceData
         self.deviceDataCollector = deviceDataCollector;
     }];
-
-    if([options[@"vaultManager"] boolValue]){
+    
+    if ([options[@"vaultManager"] boolValue]) {
         request.vaultManager = YES;
     }
-
-    if([options[@"cardDisabled"] boolValue]){
+    
+    if ([options[@"cardDisabled"] boolValue]) {
         request.cardDisabled = YES;
     }
-
-    if([options[@"applePay"] boolValue]){
+    
+    if ([options[@"applePay"] boolValue]) {
         NSString* merchantIdentifier = options[@"merchantIdentifier"];
         NSString* countryCode = options[@"countryCode"];
         NSString* currencyCode = options[@"currencyCode"];
@@ -80,7 +80,7 @@ RCT_EXPORT_METHOD(show:(NSDictionary*)options resolver:(RCTPromiseResolveBlock)r
             return;
         }
         self.braintreeClient = [[BTAPIClient alloc] initWithAuthorization:clientToken];
-
+        
         self.paymentRequest = [[PKPaymentRequest alloc] init];
         self.paymentRequest.merchantIdentifier = merchantIdentifier;
         self.paymentRequest.merchantCapabilities = PKMerchantCapability3DS;
@@ -88,51 +88,49 @@ RCT_EXPORT_METHOD(show:(NSDictionary*)options resolver:(RCTPromiseResolveBlock)r
         self.paymentRequest.currencyCode = currencyCode;
         self.paymentRequest.supportedNetworks = @[PKPaymentNetworkAmex, PKPaymentNetworkVisa, PKPaymentNetworkMasterCard, PKPaymentNetworkDiscover, PKPaymentNetworkChinaUnionPay];
         self.paymentRequest.paymentSummaryItems =
-            @[
-                [PKPaymentSummaryItem summaryItemWithLabel:merchantName amount:orderTotal]
-            ];
-
+        @[
+            [PKPaymentSummaryItem summaryItemWithLabel:merchantName amount:orderTotal]
+        ];
+        
         self.viewController = [[PKPaymentAuthorizationViewController alloc] initWithPaymentRequest: self.paymentRequest];
         self.viewController.delegate = self;
-    }else{
+    } else {
         request.applePayDisabled = YES;
     }
     
-    if(![options[@"payPal"] boolValue]){ //disable paypal
+    if (![options[@"payPal"] boolValue]) { //disable paypal
         request.paypalDisabled = YES;
     }
-
+    
     BTDropInController *dropIn = [[BTDropInController alloc] initWithAuthorization:clientToken request:request handler:^(BTDropInController * _Nonnull controller, BTDropInResult * _Nullable result, NSError * _Nullable error) {
-            [self.reactRoot dismissViewControllerAnimated:YES completion:nil];
-
-            //result.paymentOptionType == .ApplePay
-            //NSLog(@"paymentOptionType = %ld", result.paymentOptionType);
-
-            if (error != nil) {
-                reject(error.localizedDescription, error.localizedDescription, error);
-            } else if (result.cancelled) {
-                reject(@"USER_CANCELLATION", @"The user cancelled", nil);
-            } else {
-                if (threeDSecureOptions && [result.paymentMethod isKindOfClass:[BTCardNonce class]]) {
-                    BTCardNonce *cardNonce = (BTCardNonce *)result.paymentMethod;
-                    if (!cardNonce.threeDSecureInfo.liabilityShiftPossible && cardNonce.threeDSecureInfo.wasVerified) {
-                        reject(@"3DSECURE_NOT_ABLE_TO_SHIFT_LIABILITY", @"3D Secure liability cannot be shifted", nil);
-                    } else if (!cardNonce.threeDSecureInfo.liabilityShifted && cardNonce.threeDSecureInfo.wasVerified) {
-                        reject(@"3DSECURE_LIABILITY_NOT_SHIFTED", @"3D Secure liability was not shifted", nil);
-                    } else{
-                        [[self class] resolvePayment:result deviceData:self.deviceDataCollector resolver:resolve];
-                    }
-                } else if(result.paymentMethod == nil && (result.paymentOptionType == 16 || result.paymentOptionType == 18)){ //Apple Pay
-                    // UIViewController *ctrl = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-                    // [ctrl presentViewController:self.viewController animated:YES completion:nil];
-                    UIViewController *rootViewController = RCTPresentedViewController();
-                    [rootViewController presentViewController:self.viewController animated:YES completion:nil];
+        [self.reactRoot dismissViewControllerAnimated:YES completion:nil];
+        
+        //result.paymentOptionType == .ApplePay
+        NSLog(@"paymentOptionType = %ld", result.paymentMethodType);
+        
+        if (error != nil) {
+            reject(error.localizedDescription, error.localizedDescription, error);
+        } else if (result.canceled) {
+            reject(@"USER_CANCELLATION", @"The user cancelled", nil);
+        } else {
+            if (threeDSecureOptions && [result.paymentMethod isKindOfClass:[BTCardNonce class]]) {
+                BTCardNonce *cardNonce = (BTCardNonce *)result.paymentMethod;
+                if (!cardNonce.threeDSecureInfo.liabilityShiftPossible && cardNonce.threeDSecureInfo.wasVerified) {
+                    reject(@"3DSECURE_NOT_ABLE_TO_SHIFT_LIABILITY", @"3D Secure liability cannot be shifted", nil);
+                } else if (!cardNonce.threeDSecureInfo.liabilityShifted && cardNonce.threeDSecureInfo.wasVerified) {
+                    reject(@"3DSECURE_LIABILITY_NOT_SHIFTED", @"3D Secure liability was not shifted", nil);
                 } else{
                     [[self class] resolvePayment:result deviceData:self.deviceDataCollector resolver:resolve];
                 }
+            } else if (result.paymentMethod == nil && result.paymentMethodType == BTDropInPaymentMethodTypeApplePay) { //Apple Pay
+                UIViewController *rootViewController = RCTPresentedViewController();
+                [rootViewController presentViewController:self.viewController animated: YES completion:nil];
+            } else{
+                [[self class] resolvePayment:result deviceData:self.deviceDataCollector resolver:resolve];
             }
-        }];
-
+        }
+    }];
+    
     if (dropIn != nil) {
         [self.reactRoot presentViewController:dropIn animated:YES completion:nil];
     } else {
@@ -140,11 +138,13 @@ RCT_EXPORT_METHOD(show:(NSDictionary*)options resolver:(RCTPromiseResolveBlock)r
     }
 }
 
+// Apple Pay
+// Implement PKPaymentAuthorizationViewControllerDelegate
 - (void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller
                        didAuthorizePayment:(PKPayment *)payment
                                 completion:(void (^)(PKPaymentAuthorizationStatus))completion
 {
-
+    
     // Example: Tokenize the Apple Pay payment
     BTApplePayClient *applePayClient = [[BTApplePayClient alloc]
                                         initWithAPIClient:self.braintreeClient];
@@ -155,23 +155,22 @@ RCT_EXPORT_METHOD(show:(NSDictionary*)options resolver:(RCTPromiseResolveBlock)r
             // On success, send nonce to your server for processing.
             // If applicable, address information is accessible in `payment`.
             // NSLog(@"description = %@", tokenizedApplePayPayment.localizedDescription);
-
+            
             completion(PKPaymentAuthorizationStatusSuccess);
             self.applePayAuthorized = YES;
-
-
+            
+            
             NSMutableDictionary* result = [NSMutableDictionary new];
             [result setObject:tokenizedApplePayPayment.nonce forKey:@"nonce"];
             [result setObject:@"Apple Pay" forKey:@"type"];
             [result setObject:[NSString stringWithFormat: @"%@ %@", @"", tokenizedApplePayPayment.type] forKey:@"description"];
             [result setObject:[NSNumber numberWithBool:false] forKey:@"isDefault"];
             [result setObject:self.deviceDataCollector forKey:@"deviceData"];
-
+            
             self.resolve(result);
-
+            
         } else {
             // Tokenization failed. Check `error` for the cause of the failure.
-
             // Indicate failure via the completion callback:
             completion(PKPaymentAuthorizationStatusFailure);
         }
@@ -181,38 +180,38 @@ RCT_EXPORT_METHOD(show:(NSDictionary*)options resolver:(RCTPromiseResolveBlock)r
 // Be sure to implement -paymentAuthorizationViewControllerDidFinish:
 - (void)paymentAuthorizationViewControllerDidFinish:(PKPaymentAuthorizationViewController *)controller{
     [self.reactRoot dismissViewControllerAnimated:YES completion:nil];
-    if(self.applePayAuthorized == NO){
+    if (self.applePayAuthorized == NO){
         self.reject(@"USER_CANCELLATION", @"The user cancelled", nil);
     }
 }
 
 + (void)resolvePayment:(BTDropInResult* _Nullable)result deviceData:(NSString * _Nonnull)deviceDataCollector resolver:(RCTPromiseResolveBlock _Nonnull)resolve {
     //NSLog(@"result = %@", result);
-
+    
     NSMutableDictionary* jsResult = [NSMutableDictionary new];
-
+    
     //NSLog(@"paymentMethod = %@", result.paymentMethod);
     //NSLog(@"paymentIcon = %@", result.paymentIcon);
-
+    
     [jsResult setObject:result.paymentMethod.nonce forKey:@"nonce"];
     [jsResult setObject:result.paymentMethod.type forKey:@"type"];
     [jsResult setObject:result.paymentDescription forKey:@"description"];
     [jsResult setObject:[NSNumber numberWithBool:result.paymentMethod.isDefault] forKey:@"isDefault"];
     [jsResult setObject:deviceDataCollector forKey:@"deviceData"];
-
+    
     resolve(jsResult);
 }
 
 - (UIViewController*)reactRoot {
     UIViewController *root  = [UIApplication sharedApplication].keyWindow.rootViewController;
     UIViewController *maybeModal = root.presentedViewController;
-
+    
     UIViewController *modalRoot = root;
-
+    
     if (maybeModal != nil) {
         modalRoot = maybeModal;
     }
-
+    
     return modalRoot;
 }
 
